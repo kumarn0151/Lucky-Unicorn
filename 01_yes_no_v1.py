957fe7618d2f1ae4e0a925687203fc3c0b98da90
kumarn0151 <99696234+kumarn0151@users.noreply.github.com>
2022-02-23 12:17:13 +1300

    Update 01_yes_no_v1.py
    v2

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -5,15 +5,20 @@ while show_instructions != "xxx":
 
     show_instructions = input ("Have you played this game "
                                 "before? ").lower()
-
-    # If they say yes, output 'program continues' 
-    if show_instructions == "yes":
+# if they say yes, output 'program continues'
+# if they say no, output 'display instructions'
+# if the answer is invaild, print an error.
+ 
+    if show_instructions == "yes" or show_instructions == "y":
+        show_instructions == "yes"
         print ("program continues")
 
     elif show_instructions == "y":
         print ("program continues")
 
-    elif show_instructions == "no":
+    elif show_instructions == "no" or show_instructions == "n":
+        print ("program continues")
+
         print("display instructions") 
 
     elif show_instructions == "n":  
